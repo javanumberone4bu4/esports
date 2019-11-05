f51bb6d69cf9b6fb8908c59d9f9c890b429ef7cb
Wang Xiaoping <1228068461@qq.com>
2019-11-05 22:14:45 +0800

    完善了登录注册,引入了微信登录, 进一步对接口进行优化.

---
+++ src/main/java/com/rimi/esports/controller/ExceptionController.java
@@ -0,0 +1,23 @@
+package com.rimi.esports.controller;
+
+import com.rimi.esports.common.DefaultResult;
+import com.rimi.esports.common.Result;
+import com.rimi.esports.common.ResultCode;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理
+ *
+ * @author Wang Xiaoping
+ * @date 2019/11/5 14:14
+ */
+@ControllerAdvice
+public class ExceptionController {
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public Result error(Exception e){
+        return new DefaultResult(ResultCode.ERROR);
+    }
+}
---
+++ src/main/java/com/rimi/esports/controller/SmController.java
@@ -0,0 +1,43 @@
+package com.rimi.esports.controller;
+
+import com.rimi.esports.beans.Sm;
+import com.rimi.esports.common.Result;
+import com.rimi.esports.common.ResultData;
+import com.rimi.esports.service.ISmService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 个人信息表交互
+ *
+ * @author Wang Xiaoping
+ * @date 2019/11/5 10:06
+ */
+@RestController
+@CrossOrigin
+@Api(tags = {"个人信息表操作"})
+public class SmController {
+    private ISmService smService;
+
+    public SmController(ISmService smService) {
+        this.smService = smService;
+    }
+    @PostMapping("/sm/edit")
+    @ApiOperation(value = "编辑个人信息")
+    public Result updateByTelephone(Sm sm){
+        return smService.updateByTelephone(sm);
+    }
+    @PostMapping("/sm/save")
+    @ApiOperation(value = "保存个人信息")
+    public Result save(Sm sm){
+        return smService.save(sm);
+    }
+    @PostMapping("/sm/show")
+    @ApiOperation(value = "查询个人信息")
+    public ResultData selectByTelephone(Sm sm){
+        return smService.selectByTelephone(sm);
+    }
+}
